8f860320a85115b119ac51402df63e1d045db0c3
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-02-14 11:34:13 +0300

    Updated code

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>ReactMeals</h1>
+        <h1>Nairobi Meals Street</h1>
         <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
