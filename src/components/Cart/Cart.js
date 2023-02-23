406495e7f36d046e1558aa1d6eadab9fd1f33707
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-02-23 13:54:38 +0300

    Updated code

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,6 +4,7 @@ import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
@@ -41,6 +42,7 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
+      <Checkout />
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
           Close
---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import classes from './Checkout.module.css'
+
+
+const Checkout = () => {
+    return (
+    <form>
+        <div className={classes.control}>
+            <label htmlFor='name'>Your Name</label>
+            <input type='text' id='name' />
+        </div>
+        <div className={classes.control}>
+            <label htmlFor='street'>Street</label>
+            <input type='text' id='street' />
+            </div>
+        <div className={classes.control}>
+            <label htmlFor='postal'>Postal code</label>
+            <input type='text' id='postal' />
+        </div>
+        <div className={classes.control}>
+            <label htmlFor='city'>City</label>
+            <input type='text' id='city' />
+        </div>
+        <button>Confirm</button>
+    </form>
+    )
+}
+
+export default Checkout
